c9c4cf1092278d6507fb9ffc0533f5018b29503d
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-28 08:50:41 +0100

    Version with additional test and example in main.

---
+++ src/main/java/org/futurecollars/lesson4/task1/Main.java
@@ -0,0 +1,10 @@
+package org.futurecollars.lesson4.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConsoleMessage consoleMessage = new ConsoleMessage();
+        System.out.println(consoleMessage.getMessage());
+    }
+}
---
+++ src/main/java/org/futurecollars/lesson4/task1/Message.java
@@ -0,0 +1,6 @@
+package org.futurecollars.lesson4.task1;
+
+public abstract class Message {
+
+    public abstract String getMessage();
+}
