fcba18e1faa85e87af5d220fc6befe1695c2610e
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-29 00:14:37 +0100

    Final version + new Test - MessageTest

---
+++ src/test/java/org/futurecollars/lesson4/task1/MessageTest.java
@@ -0,0 +1,30 @@
+package org.futurecollars.lesson4.task1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+
+    @Test
+    void shouldReturnConsoleMessage() {
+        //Given
+        String expextedText = "This is class ConsoleMessage";
+        ConsoleMessage consoleMessage = new ConsoleMessage();
+        //When
+        String actualText = consoleMessage.getMessage();
+        //Then
+        assertEquals(expextedText, actualText);
+    }
+
+    @Test
+    void shouldReturnWindowsMessage() {
+        //Given
+        String expextedText = "This is class WindowMessage";
+        WindowMessage windowMessage = new WindowMessage();
+        //When
+        String actualText = windowMessage.getMessage();
+        //Then
+        assertEquals(expextedText, actualText);
+    }
+}
