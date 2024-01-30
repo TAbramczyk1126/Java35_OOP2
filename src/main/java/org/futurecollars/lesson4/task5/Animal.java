51101a23dcf9da0e8e3135b0e1a9832cedc0ae50
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-30 23:11:53 +0100

    Final version

---
+++ src/main/java/org/futurecollars/lesson4/task5/Animal.java
@@ -0,0 +1,10 @@
+package org.futurecollars.lesson4.task5;
+
+class Animal {
+
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/org/futurecollars/lesson4/task5/AnimalTest.java
@@ -0,0 +1,20 @@
+package org.futurecollars.lesson4.task5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+
+    @Test
+    void shouldReturnAnimalName() {
+        // given
+        String expectedName = "Simba";
+        Animal animal = new Animal("Simba") {
+        };
+        // when
+        String result  = animal.name;
+        // then
+        assertEquals(expectedName, result);
+    }
+}
