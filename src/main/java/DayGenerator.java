09ddffb4e14cd8d9f87e88781bc6aa4d137a9b2f
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-30 23:39:42 +0100

    Final version - convert Enum to String

---
+++ src/main/java/DayGenerator.java
@@ -0,0 +1,23 @@
+public class DayGenerator {
+
+    public static String convertEnumToString(DayOfTheWeek day) {
+        switch (day) {
+            case MONDAY:
+                return "Monday";
+            case TUESDAY:
+                return "Tuesday";
+            case WEDNESDAY:
+                return "Wednesday";
+            case THURSDAY:
+                return "Thursday";
+            case FRIDAY:
+                return "Friday";
+            case SATURDAY:
+                return "Saturday";
+            case SUNDAY:
+                return "Sunday";
+            default:
+                return "Invalid day";
+        }
+    }
+}
