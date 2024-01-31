c6f61f28f20a86397e808af4428875107e1c2cbc
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-31 23:07:17 +0100

    Final version

---
+++ src/main/java/org/futurecollars/lesson4/task9/Main.java
@@ -0,0 +1,9 @@
+package org.futurecollars.lesson4.task9;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(5, 10);
+        System.out.println(triangle.toString());
+    }
+}
